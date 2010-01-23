79caca7028c38f1e826bae59c39bd3005cacdbe6
Jonas Coch <klaftertief@googlemail.com>
2010-01-23 19:24:35 +0100

    Added german translation

---
+++ lang/imagecropper.de.php
@@ -0,0 +1,23 @@
+<?php
+	$dictionary = array(
+		'Image Cropper' => 'Bildausschnitt',
+		'"%1$s" needs to have a width of at least %2$spx.' => '"%1$s" muß mindestens %2$spx breit sein.',
+		'"%1$s" needs to have a height of at least %2$spx.' => '"%1$s" muß mindestens %2$spx hoch sein.',
+		'Related upload field' => 'Zugehöriges Upload Feld',
+		'None Selected' => 'Keins ausgewählt',
+		'Aspect ratios <i>Optional</i>' => 'Seitenverhältnis <i>Optional</i>',
+		'Minimum width <i>Optional</i>' => 'Mindestbreite <i>Optional</i>',
+		'Minimum height <i>Optional</i>' => 'Mindesthöhe <i>Optional</i>',
+		'There is no upload field in this section. You have to save the section with an upload field before you can add an image cropper field.' => 'Es gibt kein Upload Feld in dieser Sektion. Bevor Sie ein Bildausschnitts-Feld hinzufügen können, müssen Sie diese Sektion mit einem Upload Feld speichern.',
+		'This is a required field.' => 'Dies ist ein Pflichtfeld',
+		'Ratios have to be well formed.' => 'Seitenverhältnisse müssen richtig formatiert sein.',
+		'This has to be an integer.' => 'Dieses muß eine Ganze Zahl sein.',
+		'Aspect ratio' => 'Seitenverhältnis',
+		'Free cropping' => 'Freies Beschneiden',
+		'Fixed at' => 'Fixiert auf',
+		'Optional' => 'Optional',
+		'Actions' => 'Aktionen',
+		'Reset' => 'Zurücksetzen',
+		'Reset all values' => 'Alle Werte zurücksetzen',
+		'Not cropped' => 'Nicht beschnitten'
+	);
