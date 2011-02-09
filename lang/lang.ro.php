0eec322cb0402dcb21cb158538e652769a457dd6
Vlad <vlad_micutul@yahoo.com>
2011-02-09 12:02:14 +0200

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,72 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad_micutul@yahoo.com',
+			'website' => ''
+		),
+		'release-date' => '2010-11-12',
+	);
+
+
+	/**
+	 * Field: Image Cropper
+	 */
+	$dictionary = array(
+
+		// Missing
+
+		'Image Cropper' => 
+		'Decupare Imagine',
+
+		'"%1$s" needs to have a width of at least %2$spx.' => 
+		'"%1$s" trebuie să fie de lungime minim %2$spx.',
+
+		'"%1$s" needs to have a height of at least %2$spx.' => 
+		'"%1$s" trebuie să fie de lăţime minim %2$spx.',
+
+		'Related upload field' => 
+		'Câmpul de încărcare corespondent',
+
+		'None Selected' => 
+		'Niciunul Selectat',
+
+		'Aspect ratios <i>Optional</i>' => 
+		'Proporţii <i>Opţional</i>',
+
+		'Minimum width <i>Optional</i>' => 
+		'Lungime minimă <i>Opţional</i>',
+
+		'Minimum height <i>Optional</i>' => 
+		'Lăţime minimă <i>Opţional</i>',
+
+		'There is no upload field in this section. You have to save the section with an upload field before you can add an image cropper field.' => 
+		'Nu există câmp de încărcare în această secţiune. Secţiunea trebuie salvată cu un câmp de upload înainte de a adăuga un Tăietor de Imagine',
+
+		'Ratios have to be well formed.' => 
+		'Proporţiile trebuie să fie bine definite.',
+
+		'This has to be an integer.' => 
+		'Trebuie să fie un întreg.',
+
+		'Aspect ratio' => 
+		'Proporţia',
+
+		'Free cropping' => 
+		'Tăiere liberă',
+
+		'Fixed at ' => 
+		'Fixată la ',
+
+		'Actions' => 
+		'Acţiuni',
+
+		'Reset' => 
+		'Resetează',
+
+		'Reset all values' => 
+		'Resetează toate valorile',
+
+	);
